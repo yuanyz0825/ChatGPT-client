b5a91360ffb72cf9cb945bf061dc838a38341f8e
stn54999 <86452426+stn54999@users.noreply.github.com>
2023-04-15 19:18:43 +0800

    Create ChatGPT.py
    检查用户输入是否为空，如果为空则不执行发送操作。
    使用 text-davinci-002 模型代替 text-davinci-003 模型（在我的示例代码中），因为 text-davinci-003 模型可能在某些情况下无法访问。实际使用时，请根据您的需求选择合适的模型。
    使用 wrap=tk.WORD 参数在文本框和输入框中启用自动换行，以提高可读性。
    优化了异常处理，避免错误信息打断正常输出。

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 import openai
-from collections import deque  # 加入deque版本号
+from collections import deque
 
-#openai.api_key = 'sk-SZMxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxbkFJgyPp9EgtB0Nr6E7QtXpc'
-openai.api_key='替换自己的key'
-history = deque(maxlen=10)  # 新增处
-
+openai.api_key = 'your_api_key'
+history = deque(maxlen=10)
 
 def send_message():
     global history
-    new_prompt = entry.get('1.0', tk.END)
-    prompt = ''.join(history) + new_prompt  # 新增处
-    history.append(f"{'请回答: '}{new_prompt}")  # 新增处
+    new_prompt = entry.get('1.0', tk.END).strip()
+    if not new_prompt:
+        return
+    prompt = ''.join(history) + new_prompt
+    history.append(f"{'请回答: '}{new_prompt}\n")
+    
     if prompt[-1] == 'Q':
         root.quit()
+    
     try:
         response = openai.Completion.create(
-            model="text-davinci-003",
+            model="text-davinci-002",
             prompt=prompt,
             temperature=0.9,
             max_tokens=2048,
@@ -24,20 +26,19 @@ def send_message():
             frequency_penalty=0,
             presence_penalty=0.6
         )
-        text.config(state='normal')  # 使框可编辑
+        
+        text.config(state='normal')
         text.insert(tk.END, "Human: " + new_prompt + "\n", 'yes')
         text.insert(tk.END, "AI: " + response['choices'][0]['text'].strip() + "\n", 'no')
-        text.tag_config('yes', foreground='blue')  # 设置文字颜色
+        text.tag_config('yes', foreground='blue')
         text.tag_config('no', foreground='red')
-        text.config(state='disabled')  # 使文本框不可编辑
-        history.append(f"{'上面问题的回答: '}{response['choices'][0]['text'].strip()}\n")  # 新增处
+        text.config(state='disabled')
+        history.append(f"{'上面问题的回答: '}{response['choices'][0]['text'].strip()}\n")
         entry.delete('1.0', tk.END)
     except Exception as e:
-
-        text.insert(tk.END, str(e), 'err')  # 设置提示文本
+        text.insert(tk.END, str(e), 'err')
         text.tag_config('err', foreground='red')
         text.insert(tk.END, "\n")
-
 
 root = tk.Tk()
 root.title("OpenAI Chatbot")
@@ -45,18 +46,18 @@ root.title("OpenAI Chatbot")
 frame = tk.Frame(root)
 frame.pack(pady=10)
 
-text = tk.Text(frame, height=15, width=50)
+text = tk.Text(frame, height=15, width=50, wrap=tk.WORD)
 text.pack(side=tk.LEFT, fill=tk.Y)
 
 scroll = tk.Scrollbar(frame)
 scroll.pack(side=tk.RIGHT, fill=tk.Y)
 
-text.configure(yscrollcommand=scroll.set)  # 加入滚动条
+text.configure(yscrollcommand=scroll.set)
 scroll.config(command=text.yview)
 
-entry = tk.Text(root, width=50, height=5)
+entry = tk.Text(root, width=50, height=5, wrap=tk.WORD)
 entry.pack(pady=5)
-entry.configure(yscrollcommand=scroll.set)  # 加入滚动条
+entry.configure(yscrollcommand=scroll.set)
 scroll.config(command=entry.yview)
 
 send = tk.Button(root, text="Send", command=send_message)
